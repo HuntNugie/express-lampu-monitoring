d457861cda9500878fdae7f3016092a9b8cc5c79
Nugie kurniawan <nugiebinhar@gmail.com>
2025-11-19 08:14:26 +0700

    controller:berhasil membuat logika untuk endpoint get data nya dan update data lampu nya

--- controllers/lampuController.js
+++ controllers/lampuController.js
@@ -1,3 +1,36 @@
-export const index = (req,res)=>{
-    res.send("berhasil")
-}
+import Lampu from "../models/Lampu.js";
+
+export const index = async (req, res) => {
+    try {
+        const data = await Lampu.findUnique({
+            where: {id: 1},
+        });
+
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(404).json({message: error});
+    }
+};
+
+export const update = async (req, res) => {
+    try {
+        const result = await Lampu.findUnique({
+            where: {id: 1},
+        });
+        let hasil;
+        if (result.status_lampu == "nyala") {
+            hasil = "mati";
+        } else {
+            hasil = "nyala";
+        }
+
+        // update
+        const update = await Lampu.update({
+            where: {id: result.id},
+            data: {status_lampu: hasil},
+        });
+        return res.status(200).json({message:`lampu ${update.status_lampu}`})
+    } catch (error) {
+        res.status(400).json({message: error});
+    }
+};
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { index } from "../controllers/lampuController.js";
+import { index, update } from "../controllers/lampuController.js";
 
 const route = Router();
 
-route.get("/all",index)
-
+route.get("/lampu/all",index)
+route.put("/lampu/update",update)
 export default route
